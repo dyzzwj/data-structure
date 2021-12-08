a5e05ae38c83b35dd2ca8c5441a8506a7a4fab8e
dyzzwj <dyzwj1998@163.com>
2021-12-08 23:13:49 +0800

    first

---
+++ src/main/java/com/dyzwj/linkedlist/SingleLinkedListDemo.java
@@ -0,0 +1,33 @@
+package com.dyzwj.linkedlist;
+
+public class SingleLinkedListDemo {
+
+
+
+    //查找单链表中的倒数第k个节点
+    //5
+
+
+}
+
+
+//带头结点的链表
+class SingleLinkedList{
+
+    //头结点
+    Node head = new Node();
+
+    //添加节点
+
+    //打印链表的数据
+
+    //添加的节点是有序的
+
+
+
+}
+
+
+class Node{
+
+}
