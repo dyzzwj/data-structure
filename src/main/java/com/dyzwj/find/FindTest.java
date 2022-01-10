5b93eeaf9096e5cbd3b388924fa42d591d6411a1
unknown <YC02167@mbcoffice.com>
2022-01-10 15:28:45 +0800

    排序算法

--- src/main/java/com/dyzwj/find/FindTest.java
+++ src/main/java/com/dyzwj/find/FindTest.java
@@ -2,6 +2,12 @@ package com.dyzwj.find;
 
 public class FindTest {
 
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,4,5,5,6,6,8,9};
+        Find find = new Find();
+        System.out.println(find.sort1(arr,5));
+
+    }
 
 }
 
@@ -10,12 +16,37 @@ class Find{
 
 
     //二分查找
+    public int sort1(int[] arr,int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while(end > start){
+            int middle = start + (end - start) / 2;
+            if(target > arr[middle]){
+                start = middle + 1;
+            }else if(target < arr[middle]){
+                end = middle - 1;
+            }else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+
+    public int sort2(int[] arr,int target){
+        int start = 0;
+        int end = arr.length - 1;
+        return 0;
+    }
+
 
     //有序数组中某个数是否存在
 
+
     //有序数组中找大于某个数最左侧的位置
 
+
     //局部最小值 数组 任意相邻的数不相等
+
 
 
 
--- src/main/java/com/dyzwj/sort/SorterTest.java
+++ src/main/java/com/dyzwj/sort/SorterTest.java
@@ -9,7 +9,7 @@ public class SorterTest{
 
         Sorter sorter = new Sorter();
         int arr[] = new int[]{1,3,5,4,6,2,0};
-        sorter.sort(arr);
+        sorter.guibing(arr);
         Arrays.stream(arr).forEach(System.out::println);
     }
 
@@ -19,8 +19,61 @@ class Sorter {
 
 
     //归并排序
+    public void guibing(int[] arr){
+
+        process(arr,0,arr.length-1);
+    }
+
+    public void process(int[] arr,int start,int end){
+        if(start == end){
+            return ;
+        }
+        int middle = start + (end -start ) /2;
+        process(arr,start,middle);
+        process(arr,middle + 1,end);
+        merge(arr,start,middle,end);
+    }
+
+    private void merge(int[] arr, int start, int middle, int end) {
+        int[] temp = new int[ end - start + 1];
+        int j = middle + 1;
+        int i = start;
+        int index = 0;
+        while (i <= middle && j <= end){
+            if(arr[i] < arr[j]){
+                temp[index] = arr[i];
+                i++;
+            }else{
+                temp[index] = arr[j];
+                j++;
+            }
+            index++;
+//            temp [index++] = arr[i] < arr[j] ? arr[i++] : arr[j++];
+        }
+
+        while (i <= middle){
+            temp[index++] = arr[i++];
+        }
+        while (j <= end) {
+            temp[index++] = arr[j++];
+        }
+        for (int i1 = 0; i1 < temp.length; i1++) {
+            arr[start++] = temp[i1];
+        }
+    }
+
 
     //冒泡排序
+    public void bubble(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr.length - i - 1; j++){
+                if(arr[j] > arr[j + 1]){
+                    swap(arr,j,j+1);
+                }
+            }
+        }
+    }
+
 
     //插入排序
     public void sort(int[] arr){
@@ -39,17 +92,32 @@ class Sorter {
 
     //i和j是一个位置的话 会出错
     private void swap(int[] arr, int i, int j) {
-        arr[i] = arr[i] ^ arr[j];
-        arr[j] = arr[i] ^ arr[j];
-        arr[i] = arr[i] ^ arr[j];
+//        arr[i] = arr[i] ^ arr[j];
+//        arr[j] = arr[i] ^ arr[j];
+//        arr[i] = arr[i] ^ arr[j];
 
-//        int temp = arr[i];
-//        arr[i] = arr[j];
-//        arr[j] = temp;
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
 
     //选择排序
+    public void choose(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            //每轮找剩余未排序的最大值对应的下标
+            int max = 0;
+            for (int j = 0; j < arr.length - i; j++) {
+                max = arr[j] > arr[max] ? j : max;
+            }
+            swap(arr,max,arr.length - i -1);
+        }
+
+    }
+
+
+
+
 
 
 
