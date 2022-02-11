0da3e36844b6d4d2441bb2a1063a1ffd0ec0cd98
unknown <YC02167@mbcoffice.com>
2022-02-11 18:02:20 +0800

    leetcode

---
+++ src/main/java/com/dyzwj/leetcode/最长公共前缀.java
@@ -0,0 +1,23 @@
+package com.dyzwj.leetcode;
+
+
+/**
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ *
+ * 如果不存在公共前缀，返回空字符串 ""。
+ */
+public class 最长公共前缀 {
+
+
+    //最长公共前缀
+    public String method(String[] strs){
+
+        String prefix = strs[0];
+        for (int i = 0; i <strs.length; i++) {
+
+        }r
+
+        return "";
+    }
+
+}
